3270ba9242c18bc2ac7e95283de62d8a3fc49ee4
Evilmoose <ronnielopez@gmail.com>
2023-10-16 18:43:22 -0700

    Utility functions and mean route implementation

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
 import express from 'express';
 
+import { 
+    convertNums, 
+    findMode, 
+    findMean, 
+    findMedian 
+} from './utility.js';
 
 const app = express();
 
 app.get('/mean', function(req, res, next) {
+
+    let numString = req.query.nums.split(',');
+    let nums = convertNums(numString);
+
+    let result = {
+        operation: "mean",
+        result: findMean(nums)
+    }
+
     return res.send(result);
 });
 
 app.get('/median', function(req, res, next) {
   
+
+
     return res.send(result);
-    
 });
 
 app.get('/mode', function(req, res, next) {
   
+    
+
     return res.send(result);
-  
-   
 });
 
 app.listen(3000, function() {
